3ab2559868ec0e440bee1e5624f1c0b27f326398
gpyh <yacine.hmito@gmail.com>
2017-08-10 14:00:43 +0200

    Init
    Squashed all previous commits in this commit.
    This way we can't recover the solution.

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const app = express();
+
+// Configure app and use your routers here
+
+module.exports = app;
---
+++ routes/authors.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const generateId = require("uuid/v4");
+
+const router = express.Router();
+
+// Define your endpoints here
+
+module.exports = router;
