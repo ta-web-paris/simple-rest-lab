d2b2df50e5e12f78394c6698a9c43ce86a8becdf
Elo <wanang.elodie@hotmail.fr>
2017-10-19 17:58:55 +0200

    PAR -David/Elodie : in progress

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
-
 const app = express();
-
+const bodyParser = require('body-parser');
 // Configure app and use your routers here
+const authorsRouter = require('./routes/authors');
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+app.use('/api/authors', authorsRouter);
 
 module.exports = app;
--- routes/authors.js
+++ routes/authors.js
@@ -1,7 +1,33 @@
 const express = require("express");
 const generateId = require("uuid/v4");
-
 const router = express.Router();
+const authors = require("../data/authors");
+
+router.get('/', (req, res) => {
+  res.json(authors);
+});
+router.post('/', (req, res) => {
+  const id = generateId();
+  const {
+    name,
+    website
+  } = req.body;
+  authors.push({
+    id,
+    name,
+    website
+  })
+  res.json(authors);
+});
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  const authorSearched = authors.filter(obj => {
+    return obj.id == id
+  });
+  res.json(authorSearched);
+})
+
 
 // Define your endpoints here
 
