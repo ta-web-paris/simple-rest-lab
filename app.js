c78a61fcf477603838ef74bb23bd674c6905d6df
Nicolas <nicolas.barthez@gmail.com>
2017-10-19 18:10:09 +0200

    Done

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 
 // Configure app and use your routers here
+const booksRouter = require("./routes/books");
+const authorsRouter = require("./routes/authors");
+
+app.use(bodyParser.json());
+app.use("/api/books", booksRouter);
+app.use("/api/authors", authorsRouter);
 
 module.exports = app;
--- routes/authors.js
+++ routes/authors.js
@@ -1,8 +1,65 @@
 const express = require("express");
 const generateId = require("uuid/v4");
+const authorsDB = require("../data/authors");
+const booksDB = require("../data/books");
 
 const router = express.Router();
 
 // Define your endpoints here
+router.get("/", (req, res, next) => {
+  res.json(authorsDB);
+});
+
+router.post("/", (req, res, next) => {
+  const id = req.body.id;
+  const name = req.body.name;
+  const website = req.body.website;
+
+  const newAuthor = {
+    id: id,
+    name: name,
+    website: website
+  };
+
+  authorsDB.push(newAuthor);
+  res.json(newAuthor);
+});
+
+router.get("/:id", (req, res, next) => {
+  const author = authorsDB.filter(el => {
+    return el.id === req.params.id;
+  });
+  res.json(author);
+});
+
+router.put("/:id", (req, res, next) => {
+  const id = req.params.id;
+
+  // First find the Author by id
+  let author = authorsDB.find(el => {
+    return el.id === id;
+  });
+
+  author = {
+    id: id,
+    name: req.body.name,
+    website: req.body.website
+  };
+  const index = authorsDB.findIndex(el => {
+    return el.id === id;
+  });
+
+  authorsDB[index] = author;
+  res.json(author);
+});
+
+router.get("/:idauthor/books", (req, res, next) => {
+  const id = req.params.idauthor;
+
+  let books = booksDB.filter(el => {
+    return el.author === id;
+  });
+  res.json(books);
+});
 
 module.exports = router;
