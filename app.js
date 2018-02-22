84ccd3f0605ba9ddc1972e5395c05061e5ca2465
Arthur M <arthur.mossa@gmail.com>
2018-02-22 16:44:47 +0100

    PAR -Arthur M and Jeff - Exercice done

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const authors = require("./routes/authors");
+const books = require("./routes/books");
 
 const app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 // Configure app and use your routers here
+app.use(bodyParser.json());
+
+app.use("/api", authors);
+app.use("/api", books);
 
 module.exports = app;
