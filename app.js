0563554fbf22e0267c96e9103160059f3f2746de
Myriam <bourbiamyriam@gmail.com>
2018-02-22 17:05:33 +0100

    Myriam&Vivian

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const authors = require("./routes/authors");
+const books = require("./routes/books");
 
 const app = express();
-
 // Configure app and use your routers here
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use("/api/authors", authors);
+app.use("/api/books", books);
 module.exports = app;
--- routes/authors.js
+++ routes/authors.js
@@ -1,8 +1,70 @@
 const express = require("express");
 const generateId = require("uuid/v4");
-
+const authors = require("../data/authors");
+const books = require("../data/books");
 const router = express.Router();
 
-// Define your endpoints here
+// GET All authors
+
+// POST New author
+router.post("/", (req, res) => {
+  const name = req.body.name;
+  const website = req.body.website;
+  const author = {
+    id: generateId(),
+    name,
+    website
+  };
+  authors.push(author), res.json(author);
+});
+
+//GET author Id
+router.get("/:authorId", (req, res) => {
+  const id = req.params.authorId;
+  const result = authors.filter(author => author.id === id);
+  res.json(result[0]);
+});
+
+//PUT authorId
+router.put("/:authorId", (req, res) => {
+  const id = req.params.authorId;
+  const result = authors.filter(author => author.id === id);
+  if (result.length === 0) {
+    const name = req.body.name;
+    const website = req.body.website;
+    const newAuthor = {
+      id: generateId(),
+      name,
+      website
+    };
+    authors.push(newAuthor);
+    res.json(newAuthor);
+  } else {
+    const name = req.body.name;
+    const website = req.body.website;
+    const newAuthor = {
+      id,
+      name,
+      website
+    };
+    authors[authors.indexOf(result[0])] = newAuthor;
+    res.json(newAuthor);
+  }
+});
+//GET books
+router.get("/:authorId/books", (req, res) => {
+  const id = req.params.authorId;
+  const result = books.filter(book => book.author === id);
+  console.log(result);
+  res.json(result);
+});
+
+//GET sort authors
+router.get("/", (req, res) => {
+  authors.sort((a, b) => {
+    return a.name.localeCompare(b.name);
+  });
+  res.json(authors);
+});
 
 module.exports = router;
